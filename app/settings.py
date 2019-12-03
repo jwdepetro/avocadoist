8168cc45dc7b23d1c165fa8d0d733285f83ec577
Jimmy DePetro <jimmy.depetro@wagwalking.com>
2019-12-02 20:42:49 -0800

    created test env file

--- app/settings.py
+++ app/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
 if 'test' in sys.argv:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'avocadoist-test'
+        'NAME': os.environ.get('DB_NAME')
     }
 
 # endregion
